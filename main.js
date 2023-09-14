e71025231d19672e5b9beb3a1a6c574ad21ed783
AkashKompelly <kompellyakash6@gmail.com>
2023-09-14 14:28:22 +0530

    editted js

--- main.js
+++ main.js
@@ -4,27 +4,6 @@ let voices = [];
 
 let voiceSelect = document.querySelector("select");
 
-function populateVoiceList() {
-  voices = window.speechSynthesis.getVoices();
-  speech.voice = voices[0];
-
-  voices.forEach((voice, i) => {
-    voiceSelect.options[i] = new Option(voice.name, i);
-  });
-}
-
-if (speechSynthesis.onvoiceschanged !== undefined) {
-  speechSynthesis.onvoiceschanged = populateVoiceList;
-} else {
-  populateVoiceList();
-}
-
-function stopSpeech() {
-  if (window.speechSynthesis.speaking) {
-    window.speechSynthesis.cancel();
-  }
-}
-
 window.speechSynthesis.onvoiceschanged = () => {
   voices = window.speechSynthesis.getVoices();
 
